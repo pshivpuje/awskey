69109e5bbf378bc95e579b09eec71bbf773ac7d4
pshivpuje <87057583+pshivpuje@users.noreply.github.com>
2022-03-17 12:13:44 +0530

    Update Aws.java

--- src/main/java/Ready/Steady/Aws.java
+++ src/main/java/Ready/Steady/Aws.java
@@ -4,7 +4,7 @@ public class Aws
 {
 	private String keyName ;
 		private String keySecrets ;
-		
+		String user = System.getProperty("PaSsWoRd");
 		public Aws(String keyName,String keySecrets) {
 			this.keyName=keyName;
 			this.keySecrets=keySecrets;
